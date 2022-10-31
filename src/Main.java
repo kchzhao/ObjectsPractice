acbfe418706cd959a150080d7a982d4433265ef6
kchzhao <kamila.chzhao@gmail.com>
2022-10-31 01:33:26 -0400

    Initial commit to branch1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        String phoneNumber = "8(916)-111-11-11";
+        System.out.println(PhoneNumberUtility.removeUselessSymbols(phoneNumber));
+        System.out.println(PhoneNumberUtility.validateCountryCode(phoneNumber));
+        String correctedPhoneNumber = PhoneNumberUtility.removeUselessSymbols(phoneNumber);
+        PhoneNumberUtility.printOperatorByPhone(correctedPhoneNumber);
 
-        System.out.println("Hello world!");
     }
 }
---
+++ src/PhoneNumberUtility.java
@@ -0,0 +1,41 @@
+public class PhoneNumberUtility {
+    public static String removeUselessSymbols(String phoneNumber) {
+        return phoneNumber.replace("-","")
+                .replace("(","")
+                .replace(")","");
+    }
+
+    public static String validateCountryCode(String phoneNumber) {
+        if (phoneNumber.startsWith("8")) {
+            return phoneNumber.replaceFirst("8","+7");
+        } else if (phoneNumber.startsWith("7")) {
+            return "+" + phoneNumber;
+        }
+        return phoneNumber;
+    }
+
+    public static void printOperatorByPhone(String phoneNumber) {
+        String operatorCode = null;
+        if (phoneNumber.startsWith("8")) {
+            operatorCode = phoneNumber.substring(1, 4);
+        } else if (phoneNumber.startsWith("+7")) {
+            operatorCode = phoneNumber.substring(2, 5);
+        }
+
+        if (operatorCode == null) {
+            System.out.println("Invalid phone number");
+            return;
+        }
+
+        switch (operatorCode){
+            case "916":
+                System.out.println("MTS");
+                break;
+            case "925":
+                System.out.println("MegaFon");
+                break;
+            case "903":
+                System.out.println("Beeline");
+        }
+    }
+}
